32e9ff1ae756af482df0513f7536989eaf725bb9
SinirliJapon <mkemalerylmz@gmail.com>
2023-05-08 16:02:08 +0200

    pokemon-info-response added

---
+++ lib/repository/pokemon_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+import 'dart:math';
+import 'package:pokedex/data/pokemon_page_response.dart';
+import 'package:http/http.dart' as http;
+
+class PokemonRepository {
+  final baseUrl = 'pokeapi.co';
+  final client = http.Client();
+
+  Future<PokemonPageResponse> getPokemonPage(int pageIndex) async {
+    final queryParameters = {
+      'limit': '200',
+      'offset': (pageIndex * 200).toString()
+    };
+
+    final uri = Uri.https(baseUrl, '/api/v2/pokemon', queryParameters);
+
+    final response = await client.get(uri);
+    final json = jsonDecode(response.body);
+
+    return PokemonPageResponse.fromJson(json);
+  }
+
+  Future<dynamic> getPokemonSpeciesInfo(int pokemonId) async {
+    final uri = Uri.https(baseUrl, '/api/v2/pokemon-species/$pokemonId');
+
+    try {
+      final response = await client.get(uri);
+      final json = jsonDecode(response.body);
+      print(json);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
