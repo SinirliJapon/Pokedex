cd257bccbc090f56c57853b6db8f3632ba024634
SinirliJapon <mkemalerylmz@gmail.com>
2023-05-15 16:06:45 +0200

    editing widget names

---
+++ lib/widgets/build_pokemon_grid_view.dart
@@ -0,0 +1,16 @@
+import 'package:pokedex/bloc/pokemon_state.dart';
+import 'package:flutter/material.dart';
+import 'package:pokedex/widgets/build_pokemon_card.dart';
+
+Widget buildPokemonGridView(
+    BuildContext context, PokemonPageLoadSuccess state) {
+  return GridView.builder(
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 3,
+    ),
+    itemCount: state.pokemon.length,
+    itemBuilder: (context, index) {
+      return buildPokemonCard(context, state.pokemon[index]);
+    },
+  );
+}
