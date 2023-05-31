100fa04aa7f27bdeebf4c47065c4bde5b2a7b8ce
SinirliJapon <mkemalerylmz@gmail.com>
2023-05-31 14:50:02 +0200

    LinearPercentIndicator added

--- lib/widgets/pokemon_stats.dart
+++ lib/widgets/pokemon_stats.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/constants/app_colors.dart';
 import 'package:pokedex/constants/stat_colors.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 Column pokemonStats(List<String> stats) {
   List<Widget> statWidgets = [];
 
+  Map<String, String> statAbbreviations = {
+    'hp': 'HP',
+    'attack': 'ATK',
+    'defense': 'DEF',
+    'special-attack': 'SP-ATK',
+    'special-defense': 'SP-DEF',
+    'speed': 'SPD',
+  };
+
   for (int i = 0; i < stats.length; i++) {
     String stat = stats[i];
     List<String> parts = stat.split(':');
-    String statName = parts[0].trim();
+    String originalStatName = parts[0].trim();
     int baseStat = int.tryParse(parts[1].trim()) ?? 0;
 
     Color statColor = statColors[i % statColors.length];
+
+    String statName = statAbbreviations[originalStatName] ?? originalStatName;
 
     statWidgets.add(
       Padding(
@@ -19,36 +31,52 @@ Column pokemonStats(List<String> stats) {
           horizontal: 16,
           vertical: 4,
         ),
-        child: Column(
+        child: Stack(
           children: [
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  statName.toUpperCase(),
-                  style: const TextStyle(
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    statName,
+                    style: const TextStyle(
                       fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: AppColors.fontGrey),
+                      fontSize: 14,
+                      color: AppColors.fontGrey,
+                    ),
+                  ),
                 ),
-                Text(
-                  baseStat.toString(),
-                  style: const TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.fontGrey,
+                Expanded(
+                  flex: 8,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: LinearPercentIndicator(
+                      animation: true,
+                      barRadius: const Radius.circular(30),
+                      padding: EdgeInsets.zero,
+                      lineHeight: 20,
+                      percent: baseStat / 300,
+                      backgroundColor: Colors.grey[300],
+                      progressColor: statColor,
+                    ),
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 4),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(30),
-              child: LinearProgressIndicator(
-                value: baseStat / 150,
-                backgroundColor: Colors.grey[300],
-                valueColor: AlwaysStoppedAnimation<Color>(statColor),
-                minHeight: 20,
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 8),
+                  child: Text(
+                    baseStat.toString(),
+                    style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.fontGrey,
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
