b55d04eb9b4c7f2cf5a9dc744c6fa8830850418e
SinirliJapon <mkemalerylmz@gmail.com>
2023-06-01 14:55:11 +0200

    searchBar edited

--- lib/screens/pokedex_view.dart
+++ lib/screens/pokedex_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex/bloc/pokemon_bloc.dart';
 import 'package:pokedex/bloc/pokemon_state.dart';
 import 'package:pokedex/constants/app_colors.dart';
+import 'package:pokedex/model/pokemon.dart';
 import 'package:pokedex/widgets/pokemon_card.dart';
 
 class PokedexView extends StatefulWidget {
@@ -15,6 +16,26 @@ class PokedexView extends StatefulWidget {
 class _PokedexViewState extends State<PokedexView> {
   TextEditingController searchController = TextEditingController();
   String searchQuery = '';
+  List<Pokemon> searchResults = []; // Temporary search results list
+  FocusNode searchFocusNode = FocusNode(); // Add a focus node
+
+  @override
+  void initState() {
+    super.initState();
+    searchFocusNode.addListener(() {
+      if (!searchFocusNode.hasFocus) {
+        // When the focus is lost, hide the keyboard
+        FocusScope.of(context).unfocus();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    searchFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,38 +59,61 @@ class _PokedexViewState extends State<PokedexView> {
           } else if (state is PokemonPageLoadSuccess) {
             return Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: searchController,
-                    onChanged: (value) {
-                      setState(() {
-                        searchQuery = value.toLowerCase();
-                      });
-                    },
-                    decoration: const InputDecoration(
-                      hintText: 'Search Pokemon',
-                      prefixIcon: Icon(Icons.search),
+                Container(
+                  color: AppColors.primarySwatch,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: Container(
+                        color: Colors.white,
+                        child: TextField(
+                          controller: searchController,
+                          focusNode: searchFocusNode, // Add the focus node
+                          onChanged: (value) {
+                            setState(() {
+                              searchQuery = value.toLowerCase();
+                              searchResults = state.pokemon
+                                  .where((pokemon) => pokemon.name
+                                      .toLowerCase()
+                                      .startsWith(searchQuery))
+                                  .toList();
+                            });
+                          },
+                          decoration: const InputDecoration(
+                            hintText: 'Search Pokemon',
+                            prefixIcon: Icon(Icons.search),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    color: AppColors.backgroundColor.withOpacity(0.7),
-                    child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
+                  child: GestureDetector(
+                    // Add a GestureDetector to handle tap gestures and hide the keyboard
+                    onTap: () {
+                      FocusScope.of(context).unfocus();
+                    },
+                    child: Container(
+                      color: AppColors.backgroundColor.withOpacity(0.7),
+                      child: GridView.builder(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                        ),
+                        itemCount: searchQuery.isEmpty
+                            ? state.pokemon.length
+                            : searchResults.length,
+                        itemBuilder: (context, index) {
+                          final pokemon = searchQuery.isEmpty
+                              ? state.pokemon[index]
+                              : searchResults[index];
+                          return buildPokemonCard(context, pokemon);
+                        },
                       ),
-                      itemCount: state.pokemon.length,
-                      itemBuilder: (context, index) {
-                        final pokemon = state.pokemon[index];
-                        if (searchQuery.isNotEmpty &&
-                            !pokemon.name.contains(searchQuery)) {
-                          return Container(); // Skip if the Pokemon doesn't match the search query
-                        }
-                        return buildPokemonCard(context, pokemon);
-                      },
                     ),
                   ),
                 ),
